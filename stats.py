c00020f6c81b6ac95c4b7bf35769b04d0500c9aa
Landon Miller <lmiller@Landons-MacBook-Pro.local>
2021-03-04 14:20:47 -0600

    Deleted out all old csv's as they were being replaced with updated ones. stats.py is complete for now. When ran it will take the appropriate jsons in certain directories, convert them to csvs, and upload them to Google Sheets / Drive.

--- stats.py
+++ stats.py
@@ -17,50 +17,33 @@
 
 import pandas as pd
 from pandas import json_normalize
+import os
+import time
 import json
-
-def FlattenJson():
-    test_dir = '/Users/lmiller/PythonProjects/MinecraftStats/proj/jsons/stats/0a1cef4e-ca8b-4392-9d82-5cd38a5e8c0a.json'
-    with open(test_dir) as f:
-        d = json.load(f)
-
-    minestats = json_normalize(d['stats'])
-    minestats.to_csv('/Users/lmiller/PythonProjects/MinecraftStats/proj/test.csv')
+import csv
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from apiclient import discovery
 
 
-# will create csv from stats jsons and return csv
+# will create csv from stats jsons and return csv -> prep work before upload
 def CreateCSV(dir_to_usercache, dir_to_jsons):
     # imports usercache.json to df
     usercache = pd.read_json(dir_to_usercache)
 
     usercache_dict = {}
 
-    stat_labels = [
-                    'broken',
-                    'crafted',
-                    'custom',
-                    'dropped',
-                    'killed',
-                    'killed_by',
-                    'mined',
-                    'picked_up',
-                    'used']
-
     # creating a df for each user and uploading to Google Sheets
     for element in range(len(usercache.index)):
 
-        # storing name in temp variable
+        # storing name for easy later acess
         name = usercache.loc[:, 'name'][element]
 
         # adding each user stats as a df to a dictionary
         usercache_dict[element] = pd.read_json(dir_to_jsons + usercache.loc[:, 'uuid'][element] + '.json')
 
-        # creating overall csv from each users dictionary
-        #usercache_dict[element].to_csv('~/PythonProjects/MinecraftStats/proj/csvs/' + name + '.csv')
-
-        # creating more detailed stats csv / user
+        # creating more detailed stats csv for each user
         for stat in range(len(usercache_dict[element])):
-            #
             detailed_stat = usercache_dict[element].loc[:, 'stats'][stat]
 
             # converting to json string
@@ -69,16 +52,107 @@ def CreateCSV(dir_to_usercache, dir_to_jsons):
             # converting to df
             stat_df = pd.read_json(stat_json, typ='series')
 
+            # renaming index and columns
+            stat_df.index.names = ['Type:']
+
+            # storing name of what the csv will be named
+            name_csv = name + '-' + usercache_dict[element].index[stat] + '.csv'
+
             # converting to csv
-            stat_df.to_csv('~/PythonProjects/MinecraftStats/proj/csvs/detailed_csvs/' + name + '-' + stat_labels[stat] + '.csv')
+            stat_df.to_csv('~/PythonProjects/MinecraftStats/proj/csvs/detailed_csvs/' + name_csv)
 
+            print('Attempting to open stats spreadsheet for...', name)
 
+            # processing the upload to google
+            GoogleUpload(name_csv, name)
+
+################################################################################
 # will take outputted csv and upload to google sheets for all to view
-def GoogleUpload(csv):
-    pass
+def GoogleUpload(name_csv, name):
 
+    # changing directory to where json is stored
+    os.chdir('/Users/lmiller/PythonProjects/MinecraftStats/')
+
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name('Minecraft-Stats-80f246126602.json', scope)
+    client = gspread.authorize(creds)
+
+    # authorize creds
+    gc = gspread.authorize(creds)
+
+    # changing directory back to proj directory
+    os.chdir('/Users/lmiller/PythonProjects/MinecraftStats/proj')
+
+    # csv directory
+    csvFile = '/Users/lmiller/PythonProjects/MinecraftStats/proj/csvs/detailed_csvs/' + name_csv
+
+    # creating google drive destination folder for spreadsheet
+    dest_folder_id = '1UvIpaqRWbxj5ueEG6IUfeqS-iKppU9t3'
+
+
+    ############################################################################
+    # attempting to open user spreadsheet -> creating one if exception is thrown
+    try:
+        minecraft_sheet = gc.open(name)
+        print('Opened spreadsheet', name)
+
+        try:
+            # updating appropriate worksheet by updating the sheet via csv import
+            minecraft_sheet.values_update(
+                            name_csv,
+                            params={'valueInputOption': 'USER_ENTERED'},
+                            body={'values': list(csv.reader(open(csvFile)))}
+            )
+            print('Update successful!')
+
+        except gspread.exceptions.APIError:
+
+            print('Worksheet not found! - Creating new worksheet with data...')
+
+            # creating new worksheet
+            minecraft_sheet.add_worksheet(title=name_csv, rows="200", cols="10")
+
+            # updating appropriate worksheet by updating the sheet via csv import
+            minecraft_sheet.values_update(
+                            name_csv,
+                            params={'valueInputOption': 'USER_ENTERED'},
+                            body={'values': list(csv.reader(open(csvFile)))}
+            )
+            print('Worksheet created and updated successfully!')
+
+    except gspread.exceptions.SpreadsheetNotFound:
+        # creating new spreadsheet for new player in minecraft stats proj folder
+        print('Spreadsheet did not exist for', name, 'Creating and opening one for them now.')
+        drive_service = discovery.build('drive', 'v3', credentials = creds)
+        file_metadata = {
+                        'name': name,
+                        'mimeType': 'application/vnd.google-apps.spreadsheet',
+                        'parents': [dest_folder_id]
+                        }
+        file = drive_service.files().create(body=file_metadata).execute()
+        minecraft_sheet = gc.open(name)
+        print('Created and opened spreadsheet!')
+
+        print('Creating new worksheet and updating with data...')
+        # creating new worksheet
+        minecraft_sheet.add_worksheet(title=name_csv, rows="200", cols="10")
+
+        # updating appropriate worksheet by updating the sheet via csv import
+        minecraft_sheet.values_update(
+                        name_csv,
+                        params={'valueInputOption': 'USER_ENTERED'},
+                        body={'values': list(csv.reader(open(csvFile)))}
+        )
+        print('Worksheet created and updated successfully!')
+
+    print('-------------------------------------------------------------------')
+    # sleeping 3 seconds to avoid quota limit
+    time.sleep(3)
+
+################################################################################
+# initializing the program
 if __name__ == '__main__':
     dir_to_usercache = '~/PythonProjects/MinecraftStats/proj/jsons/usercache.json'
     dir_to_jsons = '~/PythonProjects/MinecraftStats/proj/jsons/stats/'
     CreateCSV(dir_to_usercache, dir_to_jsons)
-    #FlattenJson()
